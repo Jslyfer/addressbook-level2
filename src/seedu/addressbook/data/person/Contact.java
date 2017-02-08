f0fe4402bbb3e37e7cae03e7a5cedeb703f6197c
Jslyfer <johannwjg@gmail.com>
2017-02-08 14:39:39 +0800

    Extended Email, Phone and Address classes to Contact Edited AdaptedPerson, AddCommandTest and ParserTest values to toString()

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Represents a Person's contact in the address book.
+ */
+
+public class Contact {
+    
+    protected String value;
+    protected boolean isPrivate;
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
